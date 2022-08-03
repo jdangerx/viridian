7e9dcd05a97a489772b74c0ee2b581a451908897
Dazhong Xia <john.danger.xia@gmail.com>
2022-08-03 13:07:19 -0400

    Better colors and shadows
    TODO: the shadow on the circle border is on the scallops, kind of janky

--- midautumn2022/sketch.js
+++ midautumn2022/sketch.js
@@ -3,6 +3,9 @@ let mask;
 let scallop;
 let patternSize;
 let scallopSize;
+let DOUGH_RGB;
+let HIGHLIGHT_RGB;
+let SHADOW_RGB;
 
 let t = 0;
 
@@ -29,50 +32,84 @@ function addHoldTime(t, prehold, duration) {
 function setup() {
     createCanvas(800, 600);
     patternSize = 200;
+
+    DOUGH_RGB = color(210, 140, 50);
+    HIGHLIGHT_RGB = color(250, 180, 80);
+    SHADOW_RGB = color(150, 100, 30);
     scallopSize = patternSize * 1.5;
     pattern = createGraphics(patternSize, patternSize);
-    mask = createGraphics(patternSize, patternSize);
+    mask = createGraphics(patternSize * 1.2, patternSize * 1.2);
     scallop = createGraphics(scallopSize, scallopSize);
-    scallop.noStroke();
-
-    // scallop could be done with p5.polar
-    scallop.background('rgba(0, 0, 0, 0)')
-    scallop.fill(200);
-    const numScallops = 8;
-    const stepSize = TAU / numScallops;
-    const ringRadius = patternSize / 2;
-    for (let i = 0; i < numScallops; i++) {
-        const diameter = TAU * ringRadius / numScallops * 1.5;
-        x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
-        y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
-        scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter);
-    }
-    scallop.fill(240);
-    scallop.circle(scallopSize / 2, scallopSize / 2, 2 * ringRadius * 1.1);
+    makeScallops();
     mask.background('rgba(0, 0, 0, 0)')
     mask.fill('rgba(0, 0, 0, 1)')
     mask.circle(patternSize / 2, patternSize / 2, patternSize);
     mask.drawingContext.globalCompositeOperation = 'source-in';
 }
 
-function draw() {
-    background(0);
-    t = frameToTime(frameCount)
-    pattern.background(200);
-    pattern.stroke(240);
-    pattern.strokeWeight(6);
-    pattern.strokeCap(SQUARE);
-    eased = lerp(0, 200, smoothstep(0.2, 0.8, t));
+function makeScallops() {
+    // scallop could be done with p5.polar
+    // should we pass in the graphics element, instead of mutating global state?
+    scallop.noStroke();
+    scallop.background('rgba(0, 0, 0, 0)')
+    const numScallops = 30;
+    const stepSize = TAU / numScallops;
+    const ringRadius = patternSize / 2;
+    scallop.fill(SHADOW_RGB);
+    for (let i = 0; i < numScallops; i++) {
+        const diameter = TAU * ringRadius / numScallops * 1.5;
+        x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+        y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+        scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter * 1.2);
+    }
+    scallop.push();
+    scallop.fill(DOUGH_RGB);
+    for (let i = 0; i < numScallops; i++) {
+        const diameter = TAU * ringRadius / numScallops * 1.5;
+        x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+        y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+        scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter);
+    }
+    scallop.pop();
+    scallop.circle(scallopSize / 2 + 3, scallopSize / 2 + 3, patternSize);
+}
+
+
+function waffle() {
+    pattern.circle(patternSize / 2, patternSize / 2, patternSize - 5);
     for (let i = 0; i < 10; i++) {
         x = 10 * (2 * i + 1)
         pattern.line(x, 0, x, eased);
         pattern.line(0, x, eased, x);
     }
+}
+function draw() {
+    background(200, 200, 240);
+    t = frameToTime(frameCount)
+    eased = lerp(0, patternSize, smoothstep(0.2, 0.8, t));
+
+    pattern.background(DOUGH_RGB);
+    pattern.strokeWeight(6);
+    pattern.strokeCap(SQUARE);
+    pattern.fill('rgba(0, 0, 0, 0)');
+
+    pattern.push();
+    pattern.translate(3, 3);
+    pattern.stroke(SHADOW_RGB);
+    waffle()
+    pattern.pop();
+
+    pattern.stroke(HIGHLIGHT_RGB);
+    pattern.circle(patternSize / 2, patternSize / 2, patternSize - 5);
+    waffle()
+
     mask.image(pattern, 0, 0);
     image(scallop, mouseX - scallopSize / 2, mouseY - scallopSize / 2);
     image(mask, mouseX - patternSize / 2, mouseY - patternSize / 2);
 }
 
+
+// probably we should move these util functions into our own little lib
 // rachel found these easing functions somewhere, I forget where. Perlin?
 function getBias(time, bias) {
     return (time / ((((1.0 / bias) - 2.0) * (1.0 - time)) + 1.0));
