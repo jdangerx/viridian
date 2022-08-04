b6b9563e2e5b6c9a4e8f4fab34733800f480e144
Dazhong Xia <john.danger.xia@gmail.com>
2022-08-03 22:21:22 -0400

    scene manager for switching animations

--- midautumn2022/mooncakes.js
+++ midautumn2022/mooncakes.js
@@ -1,4 +1,100 @@
-const mooncakes = {
+function Mooncakes() {
+    let pattern;
+    let mask;
+    let scallop;
+    let patternSize;
+    let scallopSize;
+    let DOUGH_RGB;
+    let HIGHLIGHT_RGB;
+    let SHADOW_RGB;
+    let scribble;
+    let t = 0;
 
+    this.setup = function () {
+        patternSize = 200;
 
+        DOUGH_RGB = color(210, 140, 50);
+        HIGHLIGHT_RGB = color(250, 180, 80);
+        SHADOW_RGB = color(150, 100, 30);
+        scallopSize = patternSize * 1.5;
+        pattern = createGraphics(patternSize, patternSize);
+        scribble = new Scribble(pattern);
+        scribble.bowing = 0.2;
+        scribble.roughness = 0.4;
+        mask = createGraphics(patternSize * 1.2, patternSize * 1.2);
+        scallop = createGraphics(scallopSize, scallopSize);
+        this.makeScallops();
+        mask.background('rgba(0, 0, 0, 0)')
+        mask.fill('rgba(0, 0, 0, 1)')
+        mask.circle(patternSize / 2, patternSize / 2, patternSize);
+        mask.drawingContext.globalCompositeOperation = 'source-in';
+    }
+
+    this.makeScallops = function () {
+        // scallop could be done with gyrate
+        // should we pass in the graphics element, instead of mutating global state?
+        scallop.noStroke();
+        scallop.background('rgba(0, 0, 0, 0)')
+        const numScallops = 30;
+        const stepSize = TAU / numScallops;
+        const ringRadius = patternSize / 2;
+        scallop.fill(SHADOW_RGB);
+        for (let i = 0; i < numScallops; i++) {
+            const diameter = TAU * ringRadius / numScallops * 1.5;
+            x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+            y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+            scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter * 1.2);
+        }
+        scallop.push();
+        scallop.fill(DOUGH_RGB);
+        for (let i = 0; i < numScallops; i++) {
+            const diameter = TAU * ringRadius / numScallops * 1.5;
+            x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+            y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
+            scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter);
+        }
+        scallop.pop();
+        scallop.circle(scallopSize / 2 + 3, scallopSize / 2 + 3, patternSize);
+    }
+
+    this.flower = function (ctx, frame) {
+        t = utils.frameToTime(frame, 300, 0, 1);
+        ctx.push();
+        eased = lerp(20, 40, 0.5 + 0.5 * sin(2 * TAU * t));
+        ctx.strokeWeight(4);
+
+        scribble.scribbleEllipse(100, 100, 80, 80);
+        scribble.scribbleEllipse(100, 100, 60, 60);
+        scribble.scribbleEllipse(100, 100, 40, 40);
+
+        unit = () => {
+            scribble.scribbleEllipse(0, 75, eased, 50);
+        };
+        num = 32;
+        utils.gyrate(ctx, unit, [], createVector(100, 100), num, TAU / num);
+        ctx.pop();
+    }
+
+    this.draw = function () {
+        background(200, 200, 240);
+
+        pattern.background(DOUGH_RGB);
+        pattern.strokeWeight(6);
+        pattern.strokeCap(SQUARE);
+        pattern.fill('rgba(0, 0, 0, 0)');
+
+        pattern.push();
+        pattern.translate(3, 3);
+        pattern.stroke(SHADOW_RGB);
+        this.flower(pattern, frameCount);
+        pattern.pop();
+
+        pattern.stroke(HIGHLIGHT_RGB);
+        pattern.circle(patternSize / 2, patternSize / 2, patternSize - 5);
+        this.flower(pattern, frameCount);
+
+        mask.image(pattern, 0, 0);
+        image(scallop, mouseX - scallopSize / 2, mouseY - scallopSize / 2);
+        image(mask, mouseX - patternSize / 2, mouseY - patternSize / 2);
+    }
 }
--- midautumn2022/sketch.js
+++ midautumn2022/sketch.js
@@ -1,101 +1,37 @@
-let pattern;
-let mask;
-let scallop;
-let patternSize;
-let scallopSize;
-let DOUGH_RGB;
-let HIGHLIGHT_RGB;
-let SHADOW_RGB;
-let scribble;
-let t = 0;
+/**
+ * To add a new scene:
+ * 
+ * 1. make a new Scene() function (see mooncakes.js). It should have its own
+ *    setup and draw methods and live in its own file.
+ * 2. Include that file in the <head> tag in index.html.
+ * 3. Call mgr.addScene(YourScene) to the setup in this file.
+ * 4. add a case in keyPressed so you can switch at leisure.
+ */
+let mgr;
 
 function setup() {
     createCanvas(800, 600);
     patternSize = 200;
 
-    DOUGH_RGB = color(210, 140, 50);
-    HIGHLIGHT_RGB = color(250, 180, 80);
-    SHADOW_RGB = color(150, 100, 3);
-    scallopSize = patternSize * 1.25;
-    pattern = createGraphics(patternSize, patternSize);
-    scribble = new Scribble(pattern);
-    scribble.bowing = 0.2;
-    scribble.roughness = 0.4;
-    mask = createGraphics(patternSize * 1.2, patternSize * 1.2);
-    scallop = createGraphics(scallopSize, scallopSize);
-    makeScallops();
-    mask.background('rgba(0, 0, 0, 0)')
-    mask.fill('rgba(0, 0, 0, 1)')
-    mask.circle(patternSize / 2, patternSize / 2, patternSize);
-    mask.drawingContext.globalCompositeOperation = 'source-in';
+    mgr = new SceneManager();
+    mgr.addScene(Mooncakes);
+
+    mgr.showNextScene();
 }
-
-function makeScallops() {
-    // scallop could be done with gyrate
-    // should we pass in the graphics element, instead of mutating global state?
-    scallop.noStroke();
-    scallop.background('rgba(0, 0, 0, 0)')
-    const numScallops = 30;
-    const stepSize = TAU / numScallops;
-    const ringRadius = patternSize / 2;
-    scallop.fill(SHADOW_RGB);
-    for (let i = 0; i < numScallops; i++) {
-        const diameter = TAU * ringRadius / numScallops * 1.5;
-        x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
-        y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
-        scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter * 1.2);
-    }
-    scallop.push();
-    scallop.fill(DOUGH_RGB);
-    for (let i = 0; i < numScallops; i++) {
-        const diameter = TAU * ringRadius / numScallops * 1.5;
-        x = cos(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
-        y = sin(i * stepSize) * (ringRadius - 2 * diameter / numScallops);
-        scallop.circle(scallopSize / 2 + x, scallopSize / 2 + y, diameter);
-    }
-    scallop.pop();
-    scallop.circle(scallopSize / 2 + 3, scallopSize / 2 + 3, patternSize);
-}
-
-
-function flower(ctx) {
-    t = utils.frameToTime(frameCount, 300, 0, 1);
-    ctx.push();
-    eased = lerp(20, 40, 0.5 + 0.5 * sin(2 * TAU * t));
-    ctx.strokeWeight(4);
-
-    scribble.scribbleEllipse(100, 100, 80, 80);
-    scribble.scribbleEllipse(100, 100, 60, 60);
-    scribble.scribbleEllipse(100, 100, 40, 40);
-
-    unit = () => {
-        scribble.scribbleEllipse(0, 75, eased, 50);
-    };
-    num = 32;
-    utils.gyrate(ctx, unit, [], createVector(100, 100), num, TAU / num);
-    ctx.pop();
-}
-
 
 function draw() {
-    background(200, 200, 240);
+    mgr.draw();
+}
 
-    pattern.background(DOUGH_RGB);
-    pattern.strokeWeight(6);
-    pattern.strokeCap(SQUARE);
-    pattern.fill('rgba(0, 0, 0, 0)');
+function keyPressed() {
+    // You can optionaly handle the key press at global level...
+    switch (key) {
+        case '1':
+            console.log('Switching to Mooncakes.')
+            mgr.showScene(Mooncakes);
+            break;
+    }
 
-    pattern.push();
-    pattern.translate(3, 3);
-    pattern.stroke(SHADOW_RGB);
-    flower(pattern)
-    pattern.pop();
-
-    pattern.stroke(HIGHLIGHT_RGB);
-    pattern.circle(patternSize / 2, patternSize / 2, patternSize - 5);
-    flower(pattern)
-
-    mask.image(pattern, 0, 0);
-    image(scallop, mouseX - scallopSize / 2, mouseY - scallopSize / 2);
-    image(mask, mouseX - patternSize / 2, mouseY - patternSize / 2);
+    // ... then dispatch via the SceneManager.
+    mgr.handleEvent("keyPressed");
 }
