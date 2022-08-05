2015c38df8fd15e9eb6a799dc997a510f1fab23b
Dazhong Xia <john.danger.xia@gmail.com>
2022-08-05 16:55:18 -0400

    looks cool, but need to deal with blending modes so they can overlap nicely

--- midautumn2022/riso-test-john.js
+++ midautumn2022/riso-test-john.js
@@ -1,33 +1,51 @@
 function RisoTestJohn() {
     this.setup = () => {
+        pixelDensity(1);
         // first draw stuff into a buffer
         this.buf = createGraphics(200, 200);
+        this.buf.noStroke()
         this.buf.fill('rgba(0, 0, 0, 0)')
-        this.buf.fill(240);
+        this.buf.fill(200, 120, 120);
         this.buf.circle(50, 50, 50);
-        this.buf.fill(80);
+        this.buf.fill(120, 240, 120);
         this.buf.circle(150, 150, 50);
-        this.buf.fill(160);
+        this.buf.fill(120, 120, 240);
         this.buf.circle(100, 100, 80);
 
         // then dither it
         threshold = 128;
-        this.dithered = ditherImage(this.buf, 'atkinson', threshold);
-
+        justReds = extractRGBChannel(this.buf, 'r')
+        this.ditheredRed = ditherImage(justReds, 'atkinson', threshold);
         // then make a Riso layer that uses this dithered image as its base
-        this.riso = new Riso('blue')
-        this.riso.noStroke();
-        this.riso.image(this.dithered, 0, 0)
+        this.red = new Riso('red')
+        this.red.noStroke();
+        this.red.image(this.ditheredRed, 0, 0);
+
+        justGreens = extractRGBChannel(this.buf, 'g')
+        ditheredGreen = ditherImage(justGreens, 'atkinson', threshold);
+        // then make a Riso layer that uses this dithered image as its base
+        this.green = new Riso('green')
+        this.green.noStroke();
+        this.green.image(ditheredGreen, 0, 0);
+
+        justBlues = extractRGBChannel(this.buf, 'b')
+        ditheredBlue = ditherImage(justBlues, 'atkinson', threshold);
+        // then make a Riso layer that uses this dithered image as its base
+        this.blue = new Riso('blue')
+        this.blue.noStroke();
+        this.blue.image(ditheredBlue, 0, 0);
 
         // in theory, we can extract the CMYK/RGB layers from the buf, and then make individual dithered bufs there, and then print them to separate riso layers
 
     }
     this.enter = () => {
-        this.setup();
     }
 
     this.draw = () => {
         background(240);
-        image(this.riso, 0, 0);
+        //image(this.ditheredRed, 0, 0)
+        image(this.red, 0, 0);
+        image(this.green, -200, -200);
+        image(this.blue, -200, 0)
     }
 }
