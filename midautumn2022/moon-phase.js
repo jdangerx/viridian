473ae2e28e8b9b9858cb1f46ebdf471bc4cd6d49
Rachel Hwang <rachel@bonfirestudios.com>
2022-08-06 14:16:16 -0700

    apply mask tech to moonphase anim

--- midautumn2022/moon-phase.js
+++ midautumn2022/moon-phase.js
@@ -1,29 +1,27 @@
 function Phases() {
-    // x and y to calculate the coordinates of the moon
-    let x = 0;
-    let y = 0;
+    let myImage;
+    let moonSize;
+    this.setup = () => {
+        createCanvas(900, 400);
 
-    // 1 is the earth and 2 is the moon
-    // d is the diameter 
-    // a is the angle bewtween the center of the moon and the center of the earth
-    let a = 0;
-    let d = width / 10;
-    let increment = 0.01;
+        moonSize = width/10;
 
-    let paperTexture;
+        this.mask = createGraphics(moonSize, moonSize);
+        this.mask.noStroke();
+        this.mask.fill('rgba(0, 0, 0, 1)');
 
-    var c;
+        this.westTerminator = createGraphics(moonSize, moonSize);
+        this.westTerminator.noStroke();
+        this.westTerminator.fill('rgba(0, 0, 0, 1)');
 
-    let buf1;
-    let buf2;
+        this.eastTerminator = createGraphics(moonSize, moonSize);
+        this.eastTerminator.noStroke();
+        this.eastTerminator.fill('rgba(0, 0, 0, 1)');
 
-    this.preload = function () {
+        myImage = loadImage('paper.jpg');
     }
 
-    this.setup = function () {
-        paperTexture = loadImage('grime.jpg');
-        //c = createCanvas(900, 600);
-        angleMode(RADIANS);
+    this.enter = () => {
     }
 
     this.draw = function () {
@@ -31,83 +29,97 @@ function Phases() {
         let bg = color(62, 114, 135, 255);
         let light = color(246, 226, 106, 255);
 
+        background(bg);
+        fill(51);
+
 
         // it's a while loop
-        background(bg);
 
         noStroke();
-        ellipseMode(CENTER);
 
-        a -= increment;
-        a %= -2 * Math.PI;
-
-        for (let i = 0; i < (width / (d * 1.5)); ++i) {
-            var x = -width / 2 + i * d * 1.5 + d;
-            var newA = a - (i * 0.4);
-            var newHeight = cos(0.01 * frameCount + i) * height / 3
+        for (let i = 0; i < (width / (moonSize * 1.5)); ++i) {
+            var x = i * moonSize * 1.5;
+            var y = height/2 - moonSize / 2 + cos(0.01 * frameCount + i) * moonSize;
 
             fill(color(32, 74, 105, 255));
             rectMode(CORNERS);
-            rect(x - d / 2, newHeight, x + d / 2, height);
+            rect(x, y+moonSize/2, x + moonSize, height);
+            ellipseMode(CORNER);
+            circle(x, y, moonSize);
 
-            this.drawMoon(x, newHeight, newA, light, dark);
+            this.drawMoon(x, y, moonSize/2, i*Math.PI);
         }
-
     }
 
-    this.drawMoon = function (x, y, a, light_color, dark_color) {
-        noStroke();
 
-        let color1 = color(0, 25, 25, 0); //red
-        let color2 = color(0, 25, 25, 0); //gray
-        let color3 = color(0, 25, 25, 0); //blue
-        let color4 = color(0, 25, 25, 0); //green
+    this.drawMoon = (x, y, r, aOffset) => {
+        const juggleMask = (mask, offset) => {
+            this.phaseMask(mask, (frameCount + offset * 300) % 300 / 300 * TAU, r);
+            mask.push();
+            // because we have to keep redrawing to the mask, we need to only use
+            // this source-in composite operation when we're actually applying the
+            // reverse mask
+            mask.drawingContext.globalCompositeOperation = 'source-in';
 
-        if (-Math.PI / 2 < a && a < 0) {
-            color3 = light_color;
-            color4 = light_color;
-            color1 = light_color;
-            color2 = dark_color;
-        } else if (-Math.PI < a && a < -Math.PI / 2) {
-            color1 = light_color;
-            color3 = dark_color;
-            color4 = dark_color;
-            color2 = dark_color;
-        } else if (-3 * Math.PI / 2 < a && a < -Math.PI) {
-            color4 = dark_color;
-            color2 = light_color;
-            color1 = dark_color;
-            color3 = dark_color;
-        } else if (-2 * Math.PI < a && a < -3 * Math.PI / 2) {
-            color4 = light_color;
-            color3 = light_color;
-            color1 = dark_color;
-            color2 = light_color;
-        } else {
-            color3 = light_color;
-            color4 = light_color;
-            color1 = light_color;
-            color2 = dark_color;
+            // because we keep editing the mask and re-applying, and Image.mask()
+            // is cumulative, we have to use a reverse mask.
+            mask.image(myImage, 0, 0);
+            mask.pop();
         }
+        juggleMask(this.mask, aOffset);
+        juggleMask(this.westTerminator, aOffset + 0.05);
+        juggleMask(this.eastTerminator, aOffset + -0.05);
+        // TODO: if we want to drop shadow this, or throw a dark circle behind it, we'll need to tint each layer darker, instead of using the whole opacity thing.
+        // if we do that, we might want to adjust some angle so the light/dark remains equal.
+        // maybe easiest to render all of these into one final buffer, then render that buffer to screen as a whole.
+        tint(255, 255 / 3);
 
-        fill(color1);
-        arc(x, y, d, d, PI / 2, 3 * PI / 2);
-        fill(color2);
-        arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
+        image(this.mask, x, y, moonSize, moonSize);
+        image(this.westTerminator, x, y, moonSize, moonSize);
+        image(this.eastTerminator, x, y, moonSize, moonSize);
+    }
 
-        let heightPhase = d;
-        let widthPhase = map(Math.cos(a), 0, 1, 0, d);
-
-        fill(color3);
-        arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
-        fill(color4);
-        arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2)
+    this.phaseMask = (ctx, a, r, color) => {
+        const phase = a / (PI / 2) | 0;
+        if (color === undefined) {
+            color == 'rgba(0, 0, 0, 1)'
+        }
+        ctx.push();
+        switch (phase) {
+            case 0: // waxing crescent
+                ctx.clear();
+                // light part
+                ctx.arc(r, r, 2 * r, 2 * r, - PI / 2, PI / 2);
+                // cut out dark part
+                ctx.blendMode(REMOVE);
+                ctx.fill('rgba(0, 0, 0, 1)');
+                ctx.arc(r, r, 2 * r * cos(a), 2 * r, -PI / 2, PI / 2);
+                break;
+            case 1: // waxing gibbous
+                ctx.clear();
+                // light east side
+                ctx.arc(r, r, 2 * r, 2 * r, - PI / 2, PI / 2);
+                // growing west side
+                ctx.arc(r, r, 2 * r * cos(a), 2 * r, PI / 2, 3 * PI / 2);
+                break;
+            case 2: // waning gibbous
+                ctx.clear();
+                // light west side
+                ctx.arc(r, r, 2 * r, 2 * r, PI / 2, 3 * PI / 2);
+                // shrinking east side
+                ctx.arc(r, r, 2 * r * cos(a), 2 * r, - PI / 2, PI / 2);
+                break;
+            case 3: // waning crescent
+                ctx.clear();
+                // light part
+                ctx.arc(r, r, 2 * r, 2 * r, PI / 2, 3 * PI / 2);
+                // cut out dark part
+                ctx.blendMode(REMOVE);
+                ctx.fill('rgba(0, 0, 0, 1)');
+                ctx.arc(r, r, 2 * r * cos(a), 2 * r, PI / 2, 3 * PI / 2);
+                break;
+        }
+        ctx.pop();
     }
 }
-
-// get surface normal:
-// given a circle center, describe point within the 2d circle, in normalized coordinates OS,
-// derive a z, coming out of the screen, pos z,
-// scale factor for z, related to x, y, equation 1 = x2 + y2 + z2 
-// sqrt(1 - x2 - y2) = z;
-// now we have camera local normal, convert to polar coordinate
+    
