8c1a0d94bf236d8c1d3409f0d3e3d6cc3b61a13a
Rachel Hwang <rachel@bonfirestudios.com>
2022-08-05 19:05:25 -0700

    experimenting with riso moon phase, probably a dead end

--- midautumn2022/riso-test.js
+++ midautumn2022/riso-test.js
@@ -16,13 +16,23 @@ function RisoTest() {
     let paperTexture;
     let blueChannel;
 
+    let lightBuf;
+    let darkBuf;
+
     var c;
 
     this.setup = function () {
-        paperTexture = loadImage('paper.jpg');
+        //paperTexture = loadImage('paper.jpg');
         c = createCanvas(900, 600);
         angleMode(RADIANS);
         console.log("setup riso scene");
+
+        pixelDensity(1);
+        this.lightBuf = new Riso('orange');
+        this.lightBuf.noStroke();
+
+        this.darkBuf = new Riso('black');
+        this.darkBuf.noStroke();
     }
 
     this.draw = function () {
@@ -31,26 +41,16 @@ function RisoTest() {
         // it's a while loop
         background(bg_color);
 
-        pixelDensity(1);
-        blueChannel = new Riso('black', 100, 100); // instantiate riso object as blue layer
-        blueChannel.fill(128); //set opacity
-        blueChannel.rect(20, 20, 100, 100); //draw rect to blue layer
-        drawRiso(); //draw to screen
-
         noStroke();
         ellipseMode(CENTER);
 
         a -= increment;
         a %= -2 * Math.PI;
 
-        this.drawMoon(x, 0, a);
+        this.drawMoon(200, 200, a);
 
-        blendMode(OVERLAY);
-        texture(paperTexture);
-        textureMode(IMAGE);
 
-        plane(600, 600);
-
+        /*
         for (let i = 0; i < (width / (d * 1.5)); ++i) {
             var x = -width / 2 + i * d * 1.5 + d;
             var newA = a - (i * 0.4);
@@ -62,44 +62,68 @@ function RisoTest() {
 
             //this.drawMoon(x, newHeight, newA);
         }
-
+*/
     }
 
     this.drawMoon = function (x, y, a) {
-        // moon
-        //a -= 0.01;
-        //a = -Math.PI/2 + 1;
-        /*
-        a = -Math.PI + 1;
-        a = -Math.PI + 1;
-        a = -Math.PI - 1;
-        a = -3*Math.PI/2 - 1;
-*/
-        //x = Math.cos(a) * d/2 * 5/2;
-        //y = Math.sin(a) * d/2 * 5/2;
-        noStroke();
 
         let color1 = color(0, 25, 25, 0); //red
         let color2 = color(0, 25, 25, 0); //gray
         let color3 = color(0, 25, 25, 0); //blue
         let color4 = color(0, 25, 25, 0); //green
 
+        clearRiso();    
+
+        this.lightBuf.fill(255);
+        this.darkBuf.fill(255);
+
+        let heightPhase = d;
+        let widthPhase = map(Math.cos(a), 0, 1, 0, d);
+
         if (-Math.PI / 2 < a && a < 0) {
+
+            this.lightBuf.arc(x, y, d, d, PI / 2, 3 * PI / 2);
+            this.lightBuf.arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
+            this.lightBuf.arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2)
+
+            this.darkBuf.arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
+
             color3 = light_color;
             color4 = light_color;
             color1 = light_color;
             color2 = bg_color;
         } else if (-Math.PI < a && a < -Math.PI / 2) {
+            
+            this.lightBuf.arc(x, y, d, d, PI / 2, 3 * PI / 2);
+            
+            this.darkBuf.arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
+            this.darkBuf.arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
+            this.darkBuf.arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2)
+
             color1 = light_color;
             color3 = bg_color;
             color4 = bg_color;
             color2 = bg_color;
         } else if (-3 * Math.PI / 2 < a && a < -Math.PI) {
+            
+            this.lightBuf.arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
+
+            this.darkBuf.arc(x, y, d, d, PI / 2, 3 * PI / 2);
+            this.darkBuf.arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
+            this.darkBuf.arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2)
+ 
             color4 = bg_color;
             color2 = light_color;
             color1 = bg_color;
             color3 = bg_color;
         } else if (-2 * Math.PI < a && a < -3 * Math.PI / 2) {
+            
+            this.lightBuf.arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
+            this.lightBuf.arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2)
+            this.lightBuf.arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
+
+            this.darkBuf.arc(x, y, d, d, PI / 2, 3 * PI / 2);
+
             color4 = light_color;
             color3 = light_color;
             color1 = bg_color;
@@ -117,17 +141,33 @@ function RisoTest() {
         color3 = color(200);
         color4 = color(255);
 */
-        fill(color1);
-        arc(x, y, d, d, PI / 2, 3 * PI / 2);
-        fill(color2);
-        arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
+        drawRiso();
+
+        /*
+        threshold = 128;
+        this.dithered = ditherImage(this.buf1, 'atkinson', threshold);
+        image(this.buf1, x, y,  width, height);
+*/
+        /*
+        this.buf1.fill(color2);
+        this.buf1.arc(x, y, d + 1, d + 1, 3 * PI / 2, PI / 2);
 
         let heightPhase = d;
         let widthPhase = map(Math.cos(a), 0, 1, 0, d);
 
-        fill(color3);
-        arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
-        fill(color4);
-        arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2)
+        this.buf1.fill(color3);
+        this.buf1.arc(x, y, widthPhase - 2, d + 1, PI / 2, 3 * PI / 2);
+
+        threshold = 128;
+        this.dithered = ditherImage(this.buf1, 'atkinson', threshold);
+        image(this.buf1, x, y,  width, height);
+
+        this.buf1.fill(color4);
+        this.buf1.arc(x, y, widthPhase - 2, d + 1, 3 * PI / 2, PI / 2);
+
+        threshold = 128;
+        this.dithered = ditherImage(this.buf1, 'atkinson', threshold);
+        image(this.buf1, x, y,  width, height);
+        */
     }
 }
