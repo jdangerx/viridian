f22c18c744bc5d801a0d6fcfeab8adf956f92a9f
Rachel Hwang <rachel@bonfirestudios.com>
2022-08-07 07:55:42 -0700

    And drop shadow example

--- midautumn2022/glow-test.js
+++ midautumn2022/glow-test.js
@@ -4,13 +4,17 @@ function GlowTest() {
         createCanvas(400, 400);
     }
 
-    this.glow = (glowColor, bluriness) => {
+    this.glow = (glowColor, bluriness, x, y) => {
         drawingContext.shadowBlur = bluriness;
         drawingContext.shadowColor = glowColor;
+        drawingContext.shadowOffsetX = x;
+        drawingContext.shadowOffsetY = y;
     }
 
     this.noGlow = () => {
         drawingContext.shadowBlur = 0;
+        drawingContext.shadowOffsetX = 0;
+        drawingContext.shadowOffsetY = 0;
     }
 
     this.draw = () => {
@@ -18,10 +22,12 @@ function GlowTest() {
 
         fill(255);
         noStroke();
-        this.glow(color(255), 32);
+        this.glow(color(255), 32, 0, 0);
         circle(width/2, height/2, 50);
 
         this.noGlow()
+
+        this.glow(color(50), 0, 10, 10);
 
         circle(width/2+50, height/2+50, 50);
     }
