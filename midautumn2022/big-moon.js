a7ee6cbf5fe29315e162b6484320b46771a2e111
Rachel Hwang <rachel@bonfirestudios.com>
2022-08-14 00:15:03 -0700

    set setting up big moon colors

--- midautumn2022/big-moon.js
+++ midautumn2022/big-moon.js
@@ -2,24 +2,23 @@ function BigMoon() {
     let moonImage;
     let paperImage;
     let moonSize;
-    let colorMoon = color(252, 246, 200, 255);
-    let colorMoonShadow = color(210, 210, 230, 70);
 
-    let colorDark = color(2, 70, 123, 255);
+    let colorMoon;
+    let colorMoonShadow = color(210, 210, 230, 70);
+    let colorDeepRed = color(2, 70, 123, 255);
     let colorDarkTransparent = color(2, 40, 83, 105);
     let colorDarker = color(29, 47, 87, 255);
-    let colorBG = color(8, 109, 157, 255);
+    let colorBG;
     let colorFlower = color(178, 68, 89);
 
     this.setup = () => {
 
         colorMode(HSB, 360, 100, 100, 255);
 
-        colorDark = color(206, 98.4, 48.2, 255);
-        colorDarkTransparent = color(212, 97.6, 32.5, 105);
-        colorDarker = color(221, 66.7, 34.1, 255);
-        colorBG = color(199, 94.9, 61.6, 255);
-        colorFlower = color(349, 61.8, 69.8);
+        colorDeepRed = color(356, 81.8, 64.7, 255);
+        colorBG = color(23, 44.2, 94.1, 255);
+        colorOutline = color(17, 90, 42.7);
+        colorMoon = (23, 44.2, 94.1, 255);
 
         moonSize = utils.roundUpNearest10(grid * 10);
         threshold = 128;
@@ -30,8 +29,7 @@ function BigMoon() {
         paperImage = loadImage('images/paper.jpg');
         flowerImage1 = loadImage('images/flower01.png');
 
-        this.moonBase = createGraphics(moonSize+5, moonSize+5);
-        this.moonBase.noStroke();
+        this.moonBase = createGraphics(moonSize+10, moonSize+10);
         this.moonBase.fill('rgba(0, 0, 0, 1)');
 
         this.flowerLayer = createGraphics(moonSize*4, moonSize*4);
@@ -68,7 +66,7 @@ function BigMoon() {
         utils.noGlow();
         pop();
 
-        this.drawMoon(100, 100);
+        this.drawMoon(width/2-moonSize/2, height/2-moonSize/2);
 
     }
 
@@ -77,16 +75,28 @@ function BigMoon() {
 
         // Create always circular moonbase, so the texture is visible even in shadow
         this.moonBase.push();
-        this.moonBase.ellipseMode(CORNER);
-        //this.moonBase.circle(0, 0, moonSize+5);
-        //this.moonBase.drawingContext.globalCompositeOperation = 'source-in';
-        this.moonBase.image(moonImage, 0, 0, moonSize, moonSize);
+        this.moonBase.fill(255);
+        this.moonBase.circle(0, 0, moonSize+1);
+        this.moonBase.drawingContext.globalCompositeOperation = 'source-in';
+        this.moonBase.image(paperImage, 0, 0, moonSize, moonSize);
         this.moonBase.pop();
 
+        this.moonBase.noFill();
+        this.moonBase.ellipseMode(CORNER);
+        this.moonBase.stroke(colorOutline);
+        this.moonBase.strokeWeight(2);
+
+        this.moonBase.circle(0, 0, moonSize+3);
+
         push();
-        utils.glow(colorDarker, 12, 0, 0);
+        utils.glow(0, 0, 10, 10);
+        tint(colorMoon);
         image(this.moonBase, x, y, moonSize, moonSize);
         utils.noGlow();
         pop();
+
+        
+        noFill();
+        circle(x, y, moonSize);
     }
 }
