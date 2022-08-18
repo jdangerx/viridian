4ca0e9ba303f1501d3ff4dca62413285968ac769
Dazhong Xia <john.danger.xia@gmail.com>
2022-08-18 10:07:05 -0400

    still janky drop shadow

--- midautumn2022/holey-moley.js
+++ midautumn2022/holey-moley.js
@@ -24,6 +24,10 @@ function HoleyMoley() {
         const y = 2 * frameCount % height;
         const radius = 2 * grid;
         this.cutout(this.dropShadow, x + grid * 0.1, y + grid * 0.1, radius);
+        this.dropShadow.fill('rgba(0, 0, 0, 0)');
+        this.dropShadow.stroke(80);
+        this.dropShadow.strokeWeight(grid * 0.05);
+        this.dropShadow.circle(x, y, 2 * radius);
         this.cutout(this.overlay, x, y, radius);
         image(this.dropShadow, 0, 0, width, height);
         image(this.overlay, 0, 0, width, height);
