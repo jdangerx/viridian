d587d9eba0216537bd681f75758a93d6144ae012
Dazhong Xia <john.danger.xia@gmail.com>
2022-08-18 10:16:19 -0400

    omg I just had to give it a valid color lmao

--- midautumn2022/holey-moley.js
+++ midautumn2022/holey-moley.js
@@ -19,17 +19,12 @@ function HoleyMoley() {
     this.draw = () => {
         image(this.bgImage, 0, 0, width, height);
         this.overlay.background(240);
-        this.dropShadow.background(80);
         const x = frameCount % width;
         const y = 2 * frameCount % height;
         const radius = 2 * grid;
-        this.cutout(this.dropShadow, x + grid * 0.1, y + grid * 0.1, radius);
-        this.dropShadow.fill('rgba(0, 0, 0, 0)');
-        this.dropShadow.stroke(80);
-        this.dropShadow.strokeWeight(grid * 0.05);
-        this.dropShadow.circle(x, y, 2 * radius);
         this.cutout(this.overlay, x, y, radius);
-        image(this.dropShadow, 0, 0, width, height);
+        utils.glow(color(0), radius * 0.3, 0, 0);
         image(this.overlay, 0, 0, width, height);
+        utils.noGlow();
     }
 }
