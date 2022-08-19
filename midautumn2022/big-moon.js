99f53e5df6d9d713a206b84ad24cc041620e44fb
Dazhong Xia <john.danger.xia@gmail.com>
2022-08-18 23:00:59 -0400

    Fix texture scaling in Big Moon. Also, whitespace, oops.

--- midautumn2022/big-moon.js
+++ midautumn2022/big-moon.js
@@ -43,15 +43,14 @@ function BigMoon() {
         threshold = 128;
         pixelDensity(1);
 
-        for (let i = 0; i < numFlowers; ++i)
-        {
+        for (let i = 0; i < numFlowers; ++i) {
             var rotationScale = random(-1, 1);
-    
+
             var newFlower = new FallingFlower(
-                (i+random(-0.2, 0.2))*(flowerRadius * 1.1), 
-                random(-height*2, 0), 
+                (i + random(-0.2, 0.2)) * (flowerRadius * 1.1),
+                random(-height * 2, 0),
                 flowerRadius,
-                i * 0.7, 
+                i * 0.7,
                 rotationScale);
             fallingFlowers[i] = newFlower;
         }
@@ -68,11 +67,13 @@ function BigMoon() {
         bunnyImage2 = loadImage('images/rabbits/PNG/bunny-sit.png');
         bunnyImage3 = loadImage('images/rabbits/PNG/bunny-back.png');
 
-        this.moonBase = createGraphics(moonSize+this.strokeWidth+2, moonSize+this.strokeWidth+2);
+        this.moonBase = createGraphics(width, height);
         this.moonBase.fill('rgba(0, 0, 0, 1)');
         this.moonBase.ellipseMode(CORNER);
 
-        this.flowerLayer = createGraphics(grid*10, grid*10);
+        this.moonBaseMask = createGraphics(width, height);
+
+        this.flowerLayer = createGraphics(grid * 10, grid * 10);
         this.flowerLayer.noStroke();
         this.flowerLayer.fill('rgba(0, 0, 0, 1)');
 
@@ -90,31 +91,27 @@ function BigMoon() {
 
         push();
         tint(colorBG);
-        this.backgroundLayer.image(paperImage, 0, 0, width, width * (paperImage.height/paperImage.width));
+        this.backgroundLayer.image(paperImage, 0, 0, width, width * (paperImage.height / paperImage.width));
         image(this.backgroundLayer, 0, 0, width, height);
         pop();
 
         noStroke();
-        
-        for (let j = -1; j < 1; ++j)
-        {
-            for (let i = 0; i < 20; ++i)
-            {
+
+        for (let j = -1; j < 1; ++j) {
+            for (let i = 0; i < 20; ++i) {
                 var deleteRandomly = false;
-                if (j < 0)
-                {
+                if (j < 0) {
                     deleteRandomly = true;
                 }
-                var flowerX = ((i-1) + (j*0.5)) * grid*2.1;
-                var flowerY = height/2+grid*(j+1);
-                this.drawFlower(flowerX, flowerY, i + j, grid*3, deleteRandomly);
+                var flowerX = ((i - 1) + (j * 0.5)) * grid * 2.1;
+                var flowerY = height / 2 + grid * (j + 1);
+                this.drawFlower(flowerX, flowerY, i + j, grid * 3, deleteRandomly);
             }
         }
 
-        this.drawMoon(width/2, height/2-grid*.5);
+        this.drawMoon(width / 2, height / 2 - grid * .5);
 
-        for (let i = 0; i < numFlowers; ++i)
-        {
+        for (let i = 0; i < numFlowers; ++i) {
             this.updateFlower(fallingFlowers[i]);
         }
 
@@ -122,42 +119,38 @@ function BigMoon() {
         var glowStrokeWidth = this.strokeWidth + 3;
 
         this.flowerLayer.clear();
-        this.flowerLayer.image(bunnyImage1, 0, 0, grid*10, grid*10);
+        this.flowerLayer.image(bunnyImage1, 0, 0, grid * 10, grid * 10);
         var bunSize = grid * 7;
-        var bunX = width/2+grid*3.8;
-        var bunY = height/2-grid*2;
+        var bunX = width / 2 + grid * 3.8;
+        var bunY = height / 2 - grid * 2;
         utils.drawWithOutline(this.flowerLayer, bunX, bunY, colorOutline, glowStrokeWidth, bunSize);
 
         this.flowerLayer.clear();
-        this.flowerLayer.image(bunnyImage2, 0, 0, grid*12, grid*12);
+        this.flowerLayer.image(bunnyImage2, 0, 0, grid * 12, grid * 12);
         bunSize = grid * 8;
-        bunX = width/2-grid*13;
-        bunY = height/2-grid*3;
+        bunX = width / 2 - grid * 13;
+        bunY = height / 2 - grid * 3;
         utils.drawWithOutline(this.flowerLayer, bunX, bunY, colorOutline, glowStrokeWidth, bunSize);
 
-        for (let j = 1; j < 2; ++j)
-        {
-            for (let i = 0; i < 20; ++i)
-            {
-                var flowerX = ((i-1) + (j*0.5)) * grid*2.1;
-                var flowerY = height/2+grid*(j+1);
-                this.drawFlower(flowerX, flowerY, i + j, grid*3);
+        for (let j = 1; j < 2; ++j) {
+            for (let i = 0; i < 20; ++i) {
+                var flowerX = ((i - 1) + (j * 0.5)) * grid * 2.1;
+                var flowerY = height / 2 + grid * (j + 1);
+                this.drawFlower(flowerX, flowerY, i + j, grid * 3);
             }
         }
 
         this.flowerLayer.clear();
-        this.flowerLayer.image(bunnyImage3, 0, 0, grid*10, grid*10);
-        bunX = width/2-grid*2;
-        bunY = height/2-grid*0.7;
+        this.flowerLayer.image(bunnyImage3, 0, 0, grid * 10, grid * 10);
+        bunX = width / 2 - grid * 2;
+        bunY = height / 2 - grid * 0.7;
         utils.drawWithOutline(this.flowerLayer, bunX, bunY, colorOutline, glowStrokeWidth, bunSize);
 
-        for (let j = 2; j < 3; ++j)
-        {
-            for (let i = 0; i < 20; ++i)
-            {
-                var flowerX = ((i-1) + (j*0.5)) * grid*2.1;
-                var flowerY = height/2+grid*(j+1);
-                this.drawFlower(flowerX, flowerY, i + j, grid*3);
+        for (let j = 2; j < 3; ++j) {
+            for (let i = 0; i < 20; ++i) {
+                var flowerX = ((i - 1) + (j * 0.5)) * grid * 2.1;
+                var flowerY = height / 2 + grid * (j + 1);
+                this.drawFlower(flowerX, flowerY, i + j, grid * 3);
             }
         }
 
@@ -166,8 +159,7 @@ function BigMoon() {
     }
 
     this.updateFlower = () => {
-        for (let i=0; i < numFlowers; ++i)
-        {
+        for (let i = 0; i < numFlowers; ++i) {
             var f = fallingFlowers[i];
             f.y += f.velocityY;
             if (f.y > height) {
@@ -178,7 +170,7 @@ function BigMoon() {
         }
     }
 
-    this.drawFlower = (x, y, rotation, size, deleteRandomly=false) => {
+    this.drawFlower = (x, y, rotation, size, deleteRandomly = false) => {
         utils.noGlow();
 
         var ran = random(0, 1);
@@ -187,32 +179,35 @@ function BigMoon() {
             ranImage = flowerImage1;
         }
 
-        if (deleteRandomly)
-        {
+        if (deleteRandomly) {
             ran = random(0, 1);
-            if (ran > 0.35)
-            {
+            if (ran > 0.35) {
                 return;
             }
         }
 
         this.flowerLayer.clear();
-        utils.addRotatedImage(this.flowerLayer, ranImage, rotation); 
+        utils.addRotatedImage(this.flowerLayer, ranImage, rotation);
         image(this.flowerLayer, x, y, size, size);
     }
 
 
     this.drawMoon = (x, y) => {
-
-        var size = moonSize + cos(frameCount * 0.005) * grid/2;
+        var size = moonSize + cos(frameCount * 0.005) * grid / 2;
         // Create always circular moonbase, so the texture is visible even in shadow
+        this.moonBaseMask.push();
+        this.moonBaseMask.background(255);
+        this.moonBaseMask.noStroke();
+        this.moonBaseMask.blendMode(REMOVE);
+        this.moonBaseMask.fill(255);
+        this.moonBaseMask.circle(x, y, size);
+        this.moonBaseMask.pop();
 
         this.moonBase.push();
         this.moonBase.clear();
-        this.moonBase.fill(255);
-        this.moonBase.circle(this.strokeWidth, this.strokeWidth, moonSize+1);
-        this.moonBase.drawingContext.globalCompositeOperation = 'source-in';
-        this.moonBase.image(paperImage, strokeWidth, strokeWidth, moonSize*2, moonSize*2);
+        this.moonBase.image(paperImage, 0, 0, width, width * (paperImage.height / paperImage.width));
+        this.moonBase.blendMode(REMOVE);
+        this.moonBase.image(this.moonBaseMask, 0, 0)
         this.moonBase.pop();
 
         this.moonBase.push();
@@ -220,9 +215,10 @@ function BigMoon() {
         this.moonBase.stroke(colorOutline);
         this.moonBase.strokeWeight(strokeWidth);
 
-        this.moonBase.circle(this.strokeWidth, this.strokeWidth, (moonSize));
+        this.moonBase.ellipseMode(CENTER);
+        this.moonBase.circle(x, y, size + strokeWidth);
         this.moonBase.pop();
 
-        image(this.moonBase, x-size/2, y-size*0.3, size, size);
+        image(this.moonBase, 0, 0);
     }
 }
