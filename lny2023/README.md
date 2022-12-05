daedd43bc568fb4b579cb2748744afd4ef3d6b5e
Dazhong Xia <john.danger.xia@gmail.com>
2022-12-05 08:54:10 -0500

    brainstorm + wavez

---
+++ lny2023/README.md
@@ -0,0 +1,27 @@
+offline brainstorm:
+
+* ken burns on AI art [Rachel]
+* koi pond [Rachel]
+  * rabbits on shrooms [Rachel]
+* floaty clouds [Rachel]
+* hero vines [Rachel]
+
+* water blob rabbit [John]
+* white rabbit candy wrapper aesthetic? [John]
+* gate patterns [John]
+* paper cutouts racing [John]
+
+* rabbit jumping over the moon [Rachel]
+* gilded moon
+* eQuilter.com - floaty woobly rabbits in space
+
+
+pick 3-4 to bet on
+
+picking 5-6 to prototype
+
+
+colors - if they're doing more red lights, we should think about that
+
+assets to find
+research into other cultures
--- lny2023/scratch.js
+++ lny2023/scratch.js
@@ -1,8 +1,92 @@
 function Scratch() {
     this.setup = () => {
+        this.pattern = createGraphics(128, 36, WEBGL);
+        this.pattern.background(0);
+        this.pattern.noStroke();
+    }
+
+    this.getPixelValues = (graphics) => {
+        const pixDensity = graphics.pixelDensity();
+        graphics.loadPixels();
+        const w = graphics.width;
+        const h = graphics.height;
+
+        let pixelValues = [];
+        for (let i = 0; i < graphics.pixels.length; i += 4) {
+            pixelValues.push(graphics.pixels[i] + graphics.pixels[i + 1] + graphics.pixels[i + 2]);
+        }
+        pixelValues = pixelValues.map(x => x / (3 * 255));
+        console.log(pixelValues.length);
+        const pixelsChunked = [];
+        for (let j = 0; j < h * pixDensity; j++) {
+            pixelsChunked.push(pixelValues.slice(j * w * pixDensity, (j + 1) * w * pixDensity));
+        }
+
+        const averageEveryX = (arr, x) => {
+            return arr.reduce((acc, val, i) => {
+                let ret = val;
+                ii = i % x;
+                if (ii !== 0) {
+                    ret = (acc.pop() * ii + val) / (ii + 1)
+                }
+                acc.push(ret);
+                return acc;
+            }, []);
+        }
+
+        const transpose = (arr) => {
+            let xsize = arr[0].length;
+            transposed = [];
+            for (let i = 0; i < xsize; i++) {
+                transposed.push(arr.map(row => row[i]));
+            }
+            return transposed;
+        }
+
+        const xDownsampled = pixelsChunked.map(row => averageEveryX(row, pixDensity));
+
+        return transpose(transpose(xDownsampled).map(row => averageEveryX(row, pixDensity)));
+    }
+
+    this.render = (pattern) => {
+        background(200, 150, 150);
+        const pixVals = this.getPixelValues(pattern);
+        noStroke();
+        let phase, imageLoc;
+        const numRows = 22;
+        const barWidth = width / pattern.width | 0;
+        for (let j = numRows; j > 0; j--) {
+            for (let i = 0; i < width; i += barWidth) {
+                phase = (i + frameCount) * 0.02 + 0.1;
+                imageX = i / width * pattern.width | 0;
+                // if we have a bigger picture, what happens?
+                yloc = (j - (0.5 + 0.5 * sin(phase))) / numRows;
+                imageY = ((yloc * pattern.height) - 1) | 0;
+                imageVal = pixVals[imageY][imageX];
+                ysize = 0.005 + (0.04 * imageVal) + 0.002 * sin(phase * 2);
+                fill(255);
+                rect(i, height * (yloc - ysize / 2), barWidth, height * ysize);
+            }
+        }
     }
 
     this.draw = () => {
-        background(240, 230, 220);
+
+        const pattern = this.pattern;
+        pattern.push();
+        pattern.pointLight(250, 250, 250, 50, 50, 50)
+        pattern.pointLight(250, 250, 250, -50, -20, 0)
+        pattern.strokeWeight(0.5);
+        pattern.clear();
+        pattern.background(10 + 10 * (1 + sin(frameCount * 0.1)));
+        pattern.fill(255);
+        pattern.rotateX(Math.PI * sin(frameCount * 0.01));
+        pattern.rotateY(Math.PI * sin(frameCount * 0.01));
+        pattern.torus(16, 4, 12, 8);
+        pattern.pop();
+
+        image(pattern, 0, 0, 128, 36);
+        this.render(this.pattern);
+
     }
 }
