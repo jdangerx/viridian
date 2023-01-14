9c5ec33f5767dbb8bfe53394df0b0f69ffe9ee50
rahwang <rah1@uchicago.edu>
2023-01-14 18:31:59 -0500

    the motion of the ocean

--- lny2023/squiggle.js
+++ lny2023/squiggle.js
@@ -1,6 +1,6 @@
 function Squiggle() {
     this.setup = () => {
-        this.drawOnce = true;
+        this.drawOnce = false;
     }
 
     this.draw = () => {
@@ -10,14 +10,21 @@ function Squiggle() {
         }
 
         background(250, 200, 200);
+        background(0);
 
-        //this.drawWaveBundle(0, -200, width+200, height * 0.3, 300, 0);
 
-        this.drawWaveBundle(PI/12, -500, width*0.75, height * 1.0, 200, 5.2);
+        this.drawWaveBundle(0, -1000, width+400, height * 0.4, 100, 0);
+        this.drawWaveBundle(0, -400, width+400, height * 0.5, 200, 15);
 
-        //this.drawWaveBundle(0, -100, width+200, height * 0.7, 300, 0);
+        /*
+        this.drawWaveBundle(0, -200, width+200, height * 0.6, 200, 0);
 
-        //this.drawWaveBundle(-PI/6, 200, width+100, height * 1.0, 300, 17.87);
+        this.drawWaveBundle(PI/12, -500, width*0.75, height * 1.1, 200, 5.2);
+
+        this.drawWaveBundle(-PI/8, 200, width+400, height * 0.55, 200, 6);
+
+        this.drawWaveBundle(-PI/6, 500, width+400, height * 0.8, 300, 17);
+        */
     }
 
     this.drawWaveBundle = (rotation, startX, endX, startY, bumpAmp, seed) =>
@@ -32,11 +39,11 @@ function Squiggle() {
 
     this.drawWave = (startX, endX, startY, bumpAmp, seed) =>
     {
-        var lineCount = 10;
+        var lineCount = 35;
         for (var j=1; j <= lineCount; ++j)
         {
             var yOff = j * 12 + startY;
-            var xOff = 100 * sin(j * 0.3 + frameCount * 0.03) + startX; 
+            var xOff = 100 * sin(seed + j * 0.3 + frameCount * 0.01) + startX; 
             var coordinates = this.makeCoordinates(xOff, yOff, endX, bumpAmp, j, seed);
             this.drawSpline(coordinates, seed * j);
         }
@@ -46,15 +53,15 @@ function Squiggle() {
         var coordinates = [];
         var KNOT_SEPARATION = 120;
         var KNOTS = ((endX-startX) / float(KNOT_SEPARATION)) | 0;
-        var t = frameCount * 0.0;
+        var t = frameCount * 0.02;
         for (var i = 1; i <= KNOTS; ++i)
         { 
             var x = i * KNOT_SEPARATION + startX;
-            var noiseVal = noise((j* i * KNOT_SEPARATION + seed) * 0.05) * 0;
-            var noiseVal2 = ((noise(j * 0.3 * i + seed) * 2) - 1) * 0.75 * 0;
-            var smallSin = ((sin(i * KNOT_SEPARATION * 4 + t) + 1) / 2)  * 0; 
-            var bigSin = ((cos(i * KNOT_SEPARATION *0.1)+1)/2) * bigSinAmp;
-            var y = (smallSin + noiseVal + noiseVal2) * (KNOT_SEPARATION/6) + startY + bigSin;
+            var noiseVal = noise((j* i * KNOT_SEPARATION + seed) * 0.05) * 1;
+            var noiseVal2 = ((noise(j * 0.3 * i + seed) * 2) - 1) * 0.75 * 0.5;
+            var smallSin = ((sin(i * KNOT_SEPARATION * 4) + 1) / 2)  * 0.5; 
+            var bigSin = ((cos(i * KNOT_SEPARATION *0.1 + t)+1)/2) * bigSinAmp;
+            var y = (smallSin + noiseVal + noiseVal2) * (KNOT_SEPARATION/5) + startY + bigSin;
             
             // FOR SCALE 
             //var y = startY + noise(i + j + seed) * 100;
@@ -95,10 +102,10 @@ function Squiggle() {
 
         endShape();
 
-        
+        /*
         for (let i = FIRST_X_COORDINATE; i < AMOUNT_OF_COORDINATES; i += 2) {
             ellipse(coordinates[i], coordinates[i + 1], 5, 5);
-        }
+        }*/
     
     }
 }
