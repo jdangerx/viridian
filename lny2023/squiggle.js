293dadc10f5f1ca24a0df854cae2430dbc4beb91
Dazhong Xia <john.danger.xia@gmail.com>
2023-01-15 12:23:01 -0500

    Oops, try to pre-empt merge conflicts

--- lny2023/squiggle.js
+++ lny2023/squiggle.js
@@ -4,16 +4,6 @@ function Squiggle() {
 
     this.setup = () => {
         this.drawOnce = false;
-
-        const texture = PRELOADS.all.crumpledPaper;
-        this.baseTextureLayer = utils.createBaseTexture(texture, 255, 255);
-        this.moon = createGraphics(500, 500);
-        this.moonTexture = createGraphics(500, 500);
-        this.moon.background(bg_color);
-        this.moon.stroke(red);
-        this.moon.strokeWeight(3);
-        this.moon.circle(250, 250, 500);
-        utils.applyTexture(this.moon, this.moonTexture, this.baseTextureLayer);
     }
 
     this.draw = () => {
@@ -27,12 +17,11 @@ function Squiggle() {
         stroke(red);
         strokeWeight(3);
         var t = frameCount * 0.01;
-        image(this.moonTexture, width / 2 + cos(t) * 400 - this.moon.width / 2, height / 2 + sin(t) * 400 - this.moon.height / 2);
-        return;
+        circle(width/2 + cos(t) * 400, height/2 + sin(t) * 400, 500);
 
 
-        this.drawWaveBundle(0, -1000, width + 400, height * 0.4, 100, 0);
-        this.drawWaveBundle(0, -400, width + 400, height * 0.5, 200, 15);
+        this.drawWaveBundle(0, -1000, width+400, height * 0.4, 100, 0);
+        this.drawWaveBundle(0, -400, width+400, height * 0.5, 200, 15);
 
         /*
         this.drawWaveBundle(0, -200, width+200, height * 0.6, 200, 0);
@@ -45,20 +34,23 @@ function Squiggle() {
         */
     }
 
-    this.drawWaveBundle = (rotation, startX, endX, startY, bumpAmp, seed) => {
+    this.drawWaveBundle = (rotation, startX, endX, startY, bumpAmp, seed) =>
+    {
         push();
-        translate(width / 2, height / 2);
+        translate(width/2, height/2);
         rotate(rotation);
-        translate(-width / 2, -height / 2);
+        translate(-width/2, -height/2);
         this.drawWave(startX, endX, startY, bumpAmp, seed);
         pop();
     }
 
-    this.drawWave = (startX, endX, startY, bumpAmp, seed) => {
+    this.drawWave = (startX, endX, startY, bumpAmp, seed) =>
+    {
         var lineCount = 35;
-        for (var j = 1; j <= lineCount; ++j) {
+        for (var j=1; j <= lineCount; ++j)
+        {
             var yOff = j * 12 + startY;
-            var xOff = 100 * sin(seed + j * 0.3 + frameCount * 0.01) + startX;
+            var xOff = 100 * sin(seed + j * 0.3 + frameCount * 0.01) + startX; 
             var coordinates = this.makeCoordinates(xOff, yOff, endX, bumpAmp, j, seed);
             this.drawSpline(coordinates, seed * j);
         }
@@ -67,17 +59,18 @@ function Squiggle() {
     this.makeCoordinates = (startX, startY, endX, bigSinAmp, j, seed) => {
         var coordinates = [];
         var KNOT_SEPARATION = 120;
-        var KNOTS = ((endX - startX) / float(KNOT_SEPARATION)) | 0;
+        var KNOTS = ((endX-startX) / float(KNOT_SEPARATION)) | 0;
         var t = frameCount * 0.01;
-        for (var i = 1; i <= KNOTS; ++i) {
+        for (var i = 1; i <= KNOTS; ++i)
+        { 
             var x = i * KNOT_SEPARATION + startX;
-            var noiseVal = noise((j * i * KNOT_SEPARATION + seed) * 0.05) * 1;
+            var noiseVal = noise((j* i * KNOT_SEPARATION + seed) * 0.05) * 1;
             var noiseVal2 = ((noise(j * 0.3 * i + seed) * 2) - 1) * 0.75 * 0.5;
-            var smallSin = ((sin(i * KNOT_SEPARATION * 4) + 1) / 2) * 0.5;
-            var bigSin = ((cos(i * KNOT_SEPARATION * 0.1 + t) + 1) / 2) * bigSinAmp;
-            var y = (smallSin + noiseVal + noiseVal2) * (KNOT_SEPARATION / 5) + startY + bigSin;
-
-            // FOR SCALE
+            var smallSin = ((sin(i * KNOT_SEPARATION * 4) + 1) / 2)  * 0.5; 
+            var bigSin = ((cos(i * KNOT_SEPARATION *0.1 + t)+1)/2) * bigSinAmp;
+            var y = (smallSin + noiseVal + noiseVal2) * (KNOT_SEPARATION/5) + startY + bigSin;
+            
+            // FOR SCALE 
             //var y = startY + noise(i + j + seed) * 100;
 
             if ((i === 1) || (i === KNOTS)) {
@@ -120,6 +113,6 @@ function Squiggle() {
         for (let i = FIRST_X_COORDINATE; i < AMOUNT_OF_COORDINATES; i += 2) {
             ellipse(coordinates[i], coordinates[i + 1], 5, 5);
         }*/
-
+    
     }
 }
