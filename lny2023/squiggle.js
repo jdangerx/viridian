720086f85403ae8f42884d9c7c4a6e98772fbe4c
rahwang <rah1@uchicago.edu>
2023-01-15 20:05:34 -0500

    refactor part 1

--- lny2023/squiggle.js
+++ lny2023/squiggle.js
@@ -2,10 +2,30 @@ function Squiggle() {
     var bg_color = color(250, 210, 200);
     var red = color(140, 30, 0);
 
+    var KNOT_SEPARATION = 120;
+
     this.setup = () => {
         this.drawOnce = false;
+
+        // animals
         this.rabbit = PRELOADS.squiggle.rabbit;
         this.rabbitLayer = createGraphics(this.rabbit.width, this.rabbit.height);
+
+        // paper
+        const texture = PRELOADS.all.paper;
+        this.baseTextureLayer = utils.createBaseTexture(texture, 255, 155);
+
+        // WHAT IF COLOR
+        this.redTextureLayer = utils.createBaseTexture(texture, 255, 0, 0, 155);
+
+        
+
+        this.moon = createGraphics(600, 600);
+        this.moonTexture = createGraphics(600, 600);
+        this.moon.stroke(red);
+        this.moon.strokeWeight(3);
+        this.moon.circle(300, 300, 500);
+        utils.applyTexture(this.moon, this.moonTexture, this.redTextureLayer, MULTIPLY);
     }
 
     this.draw = () => {
@@ -20,11 +40,12 @@ function Squiggle() {
         stroke(red);
         strokeWeight(3);
         var t = frameCount * 0.01;
-        circle(width/2 + cos(t) * 400, height/2 + sin(t) * 400, 500);
+        var x = width/2 + cos(t) * 400 - this.moon.width / 2;
+        var y = height/2 + sin(t) * 400 - this.moon.height / 2;
+        image(this.moon, x, y, this.moon.width, this.moon.height);
 
-
-        this.drawWaveBundle(0, -1000, width+400, height * 0.4, 100, 0);
-        this.drawWaveBundle(0, -400, width+400, height * 0.5, 200, 15);
+        this.drawWaveBundle(0, -1000, width+600, height * 0.4, 100, 0);
+        this.drawWaveBundle(0, -400, width+600, height * 0.5, 200, 15);
 
         this.makeRabbit(this.rabbitLayer);
         image(this.rabbitLayer, 0, 0, this.rabbit.width, this.rabbit.height);
@@ -59,42 +80,44 @@ function Squiggle() {
         }
     }
 
-    this.makeCoordinates = (startX, startY, endX, bigSinAmp, j, seed) => {
+    this.makeCoordinates = (startX, startY, endX, bigSinAmp, j, seed) => 
+    {
         var coordinates = [];
         var KNOT_SEPARATION = 120;
         var KNOTS = ((endX-startX) / float(KNOT_SEPARATION)) | 0;
-        var t = frameCount * 0.01;
-        for (var i = 1; i <= KNOTS; ++i)
+        var t = frameCount * 0.00;
+        for (var i = 0; i < KNOTS; ++i)
         { 
-            var x = i * KNOT_SEPARATION + startX;
-            var noiseVal = noise((j* x+ seed) * 0.05) * 1;
-            var noiseVal2 = ((noise(j * 0.3 * i + seed) * 2) - 1) * 0.75 * 0.5;
-            var smallSin = ((sin(i * KNOT_SEPARATION * 4) + 1) / 2)  * 0.5; 
-            var bigSin = ((cos(i * KNOT_SEPARATION *0.1 + t)+1)/2) * bigSinAmp;
-            var y = (smallSin + noiseVal + noiseVal2) * (KNOT_SEPARATION/5) + startY + bigSin;
-            
-            // FOR SCALE 
-            //var y = startY + noise(i + j + seed) * 100;
+            var x = this.computeX(i);
+            var y = this.computeY(x, t, j, bigSinAmp, seed);
 
-            if ((i === 1) || (i === KNOTS)) {
+            if ((i === 0) || (i === KNOTS-1)) {
                 y += 300;
             }
 
-            coordinates.push(x);
-            coordinates.push(y);
+            coordinates.push(x + startX);
+            coordinates.push(y + startY);
+
+            console.log(x + " " + y);
+
         }
 
         return coordinates;
     }
 
-    this.computeX = () =>
+    this.computeX = (knotIndex) =>
     {
-        return i * KNOT_SEPARATION + startX;
+        return knotIndex * KNOT_SEPARATION;
     }
 
-    this.computeY = () =>
+    this.computeY = (x, t, lineIndex, bigSinAmp, seed) =>
     {
-        return i * KNOT_SEPARATION + startX;
+        var noiseVal = noise((lineIndex * x + seed) * 0.05) * 1;
+        var noiseVal2 = ((noise(0.00001 * x + seed) * 2) - 1) * 0.75 * 0.25;
+        var smallSin = ((sin(x * 4) + 1) / 2)  * 0.5; 
+        var bigSin = ((cos(x *0.1 + t)+1)/2) * bigSinAmp;
+        var y = (smallSin + noiseVal + noiseVal2) * (KNOT_SEPARATION/5) + bigSin;
+        return y;
     }
 
     this.drawSpline = (coordinates, seed) => {
