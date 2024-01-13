ba5598668b888499e4ac11781072ff61f04c2ff3
Dazhong Xia <john.danger.xia@gmail.com>
2024-01-13 12:46:04 -0500

    smooth operator

--- lny2024/gradient.js
+++ lny2024/gradient.js
@@ -4,7 +4,71 @@ function Gradient() {
     var n = 4;
 
     this.setup = () => {
-        noStroke();
+        // this.do_draw()
+    }
+
+    this.draw = () => this.do_draw()
+
+    this.do_draw = () => {
+        // split out from .draw() so we can inspect a static version
+        // todo: pass in framecount here?
+        background(0);
+        var size = height / (n * 3);
+        const loopPeriod = 100;
+        var loopCounter = (frameCount / loopPeriod) | 0;
+
+        const mainPhase = frameCount * 0.005;
+        const rankPhase = rank => sin((rank + loopCounter) / n) * 0.5;
+
+        // a box that sits behind the rank N box
+        // this box never moves, but has the phase of rank N+1
+        box(n, 0, mainPhase + rankPhase(n + 1), size);
+        // moving boxes
+        // count down from highest rank because it's the farthest away
+        for (let rank = n; rank > 0; rank--) {
+            const animOffset = size * fract(frameCount / loopPeriod);
+            box(rank, animOffset, rankPhase(rank) + mainPhase, size);
+        }
+
+        // outer box that never moves
+        // TODO: there might be some clever way to make this phase match up better with the inner boxes
+        box(0, 0, mainPhase, size);
+
+    }
+
+    function box(rank, animOffset, phase, size) {
+        // rank: the higher the rank, the further "in" the animation it is
+        // animOffset: how much this box has expanded from its initial pos
+        // phase: gradient phase
+        // size: width of each bar
+
+        // TODO: pass in a p5js.point for the base location
+        setGradient(
+            phase,
+            0, rank * size - animOffset, // XY
+            width / 2, size, // dimensions
+            X_AXIS
+        );
+        setGradient(
+            phase,
+            rank * size - animOffset, 0,
+            size, height,
+            Y_AXIS
+        );
+        setGradient(
+            phase,
+            0, height - ((rank + 1) * size) + animOffset,
+            width / 2, size,
+            X_AXIS
+        );
+        setGradient(
+            phase,
+            width / 2 - ((rank + 1) * size) + animOffset,
+            0,
+            size,
+            height,
+            Y_AXIS
+        );
     }
 
     function cosineGradient(dcOffset, amp, freq, phase) {
@@ -13,63 +77,13 @@ function Gradient() {
         }
     }
 
-    function palette(t, r, g, b) {
+    function gradientColor(t, r, g, b) {
         return color(r(t), g(t), b(t));
     }
 
-    this.draw = () => {
-        background(0);
-        var max_i = 0;
-        /*
-        for (var i = 0; i < n / 2; i += 1) {
-            var size = width / n;
-            var offset = (1 + sin(i / n)) * 0.5 + frameCount * 0.005;
-            offset *= 2;
-            setGradient(offset, i * size, 0, size, height * 3, Y_AXIS);
-            max_i = i;
-        }
-
-        for (var i = max_i; i < n; i += 1) {
-            var size = width / n;
-            var offset = (1 + sin((n - i - 1) / n)) * 0.5 + frameCount * 0.005;
-            offset *= 2;
-            setGradient(offset, i * size, 0, size, height * 3, Y_AXIS);
-        }*/
-
-        var size = height / (n * 3);
-        var offset = (1 + sin(1)) * 0.5 + frameCount * 0.005;
-
-        setGradient(offset, (n - 1) * size, 0, size, height * 3, Y_AXIS);
-        setGradient(offset, 0, (n - 1) * size, width / 2, size, X_AXIS);
-        setGradient(offset, width / 2 - ((n) * size), 0, size, height * 3, Y_AXIS);
-        setGradient(offset, 0, height - ((n) * size), width / 2, size, X_AXIS);
-
-        var extraOffset = 0;
-
-        for (var i = n + 2; i >= 0; --i) {
-            var offset = (1 + sin((i + extraOffset) / n)) * 0.5 + frameCount * 0.0;
-            //offset *= 0.5;
-
-            var animOffset = size * fract(frameCount * 0.01);
-
-            var x = i;
-            if (i == n + 2) {
-                x = 0;
-                animOffset = 0;
-            }
-
-            if (i > n) {
-                x = n;
-            }
-
-            setGradient(offset, 0, x * size - animOffset, width / 2, size, X_AXIS);
-            setGradient(offset, x * size - animOffset, 0, size, height, Y_AXIS);
-            setGradient(offset, 0, height - ((x + 1) * size) + animOffset, width / 2, size, X_AXIS);
-            setGradient(offset, width / 2 - ((x + 1) * size) + animOffset, 0, size, height, Y_AXIS);
-        }
-    }
 
     function setGradient(offset, x, y, w, h, axis) {
+        // TODO: pass in a position & a dimension instead of 4 numbers
         push();
         noStroke();
         const gradientRed = cosineGradient(1.178, 0.388, 1.000, 2.138)
@@ -79,7 +93,7 @@ function Gradient() {
             // Top to bottom gradient
             for (let i = y; i <= y + h; i++) {
                 let inter = map(i, y, y + h, 0, 1) + offset;
-                let c = palette(inter, gradientRed, gradientGreen, gradientBlue);
+                let c = gradientColor(inter, gradientRed, gradientGreen, gradientBlue);
                 fill(c);
                 rect(x, i, w + 1, 1);
             }
@@ -87,7 +101,7 @@ function Gradient() {
             // Left to right gradient
             for (let i = x; i <= x + w; i++) {
                 let inter = map(i, x, x + w, 0, 1) + offset * 2;
-                let c = palette(inter, gradientRed, gradientGreen, gradientBlue);
+                let c = gradientColor(inter, gradientRed, gradientGreen, gradientBlue);
                 fill(c);
                 rect(i, y, 1, h + 1)
             }
