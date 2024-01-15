7842dd71d3e0deb8b8b81f47effbce7cf0e76bde
Rachel Hwang <rah1@uchicago.edu>
2024-01-14 20:35:11 -0500

    solitare noodling

--- lny2024/solitare.js
+++ lny2024/solitare.js
@@ -4,26 +4,50 @@ function Solitare() {
     var n = 4;
     let logo1;
     let logoCopy;
+    let buffer;
+    let background_pic;
     let colorBG = color(23, 44.2, 94.1, 255);
-
+    let offset1;
+    let offset2;
 
 
     this.setup = () => {
         noStroke();
-        logo1 = loadImage('images/Dusse_Logo_wCross_horizontal.png');
+        logo1 = loadImage('images/dusse-cross-white-pink-stroke.png');
+        background_pic = loadImage('images/filled_screen.png');
         logoCopy = createGraphics(1000, 1000);
-        background(0);
-}
+        buffer = createGraphics(width*3, height*3); 
+        buffer.background(0, 0, 0, 0);
+        noiseDetail(2, 1.0);
+
+        offset1 = 0;
+        offset2 = 0;     
+    }
 
     this.draw = () => {        
+        background(0);
         // (ctx, x, y, color, glowStrokeWidth, size)
         logoCopy.image(logo1, 0, 0, logo1.width*0.1, logo1.height*0.1);
-        logoCopy.tint(0);
+        logoCopy.tint(255);
 
-        let locX = frameCount;
-        let locY = sin(frameCount*0.1) * 150;
+        var r = 100 + (sin(frameCount*0.001))*height/2;
+        var angle = frameCount*0.05;
+        offset1 += (noise(0.5*angle)-0.5);
+        offset2 += (noise(0.5*angle+10)-0.5);
 
-        utils.drawWithOutline(logoCopy, locX, locY, colorBG, 10, logoCopy.width, logoCopy.height);
+        offset1 = constrain(offset1, -2, 2);
+        offset2 = constrain(offset2, -2, 2);
+
+        var x = r * (sin(angle) + offset1 * 0.2);
+        var y = r * (cos(angle) + offset2 * 0.2);
+
+        //locX += (noise(0.15 * frameCount) - 0.5) * 30;
+        //locY += (noise(0.15 * frameCount + 100) - 0.5) * 30;
+
+        image(background_pic, 0, 0, width, height);
+        buffer.image(logoCopy, buffer.width*0.35 + x, buffer.height*0.35 + y, logoCopy.width, logoCopy.height);
+        buffer.image(logoCopy, buffer.width*0.35 + width/2 + x, buffer.height*0.35 + y, logoCopy.width, logoCopy.height);
+        image(buffer, -width-x, -height-y, width*3, height*3);
         //utils.drawWithOutline(logo1, 0, 0, colorBG, 10, logo1.width * 0.1, logo1.height * 0.1);
     }
 }
