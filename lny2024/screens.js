f313cbd911a081f5379848bccbf4c874da650aa3
Rachel Hwang <rah1@uchicago.edu>
2024-01-25 13:36:42 -0500

    work on screens

--- lny2024/screens.js
+++ lny2024/screens.js
@@ -7,10 +7,10 @@ function Screens() {
     let locY = 0;
     let z = -100;
     let t = 0;
+    let unit = width/32;
 
 
     this.setup = () => {
-        const size = 8;
         this.pattern = createGraphics(width, height, WEBGL);
         this.pattern.background(0);
 
@@ -18,37 +18,85 @@ function Screens() {
         locY = -height/2;
         background(0);
 
-        noStroke();
         background(50,50,200);
     }
 
-    this.draw = () => {
+    this.checkCoordinates = (i, j, x, y) =>
+    {
+        if (i == x && j == y)
+        {
+            return true;
+        }
+        return false;
+    }
 
+    this.isAChosenOne = (i, j) =>
+    {
+        return false;
+    }
+        /*
+        return this.checkCoordinates(i, j, 1, 2)
+            || this.checkCoordinates(i, j, 2, 1)
+            || this.checkCoordinates(i, j, 4, 2)
+            || this.checkCoordinates(i, j, 6, 3)
+            || this.checkCoordinates(i, j, 7, 1)
+            || this.checkCoordinates(i, j, 8, 3)
+            || this.checkCoordinates(i, j, 10, 2);
+    }*/
+
+    this.draw = () => {
+        stroke(255);
+        strokeWeight(3);
         t = sin(frameCount*0.002);
         t = (t + 1) * 0.5;
         t = utils.getGain(t, 0.1);
         t = (t * 2) - 1;
         background(50,50,200);
+
+        var size =unit*2;
         
         if (t > 0)
         {
-            for (var i=0; i < 20; ++i)
+            for (var i=0; i < 40; ++i)
             {
                 for (var j=0; j < 10; ++j)
                 {
-                    var size =100;
+                    fill(255,50,170);
+                    circle(i*size, j*size, size);
+
+                    if (t > 0.8 || t < -0.8)
+                    {
+                        if (this.isAChosenOne(i, j))//noise(i, j) > 0.8)
+                        {
+                            continue;
+                        }
+                    }
                     makeCircle(i*size, j*size, size); //clip with just the main canvas!      
+
+                    noFill();
+                    circle(i*size, j*size, size);   
                 }
             }
-            }
+        }
         else
-            {
-            for (var i=0; i < 20; ++i)
+        {
+            for (var i=0; i < 40; ++i)
             {
                 for (var j=0; j < 10; ++j)
                 {
-                    var size =100;
-                    makeCircle(i*size-size/2, j*size-size/2, size); //clip with just the main canvas!      
+                    fill(255,50,170);
+                    circle(i*size-size/2, j*size-size/2, size);
+                    if (t > 0.8 || t < -0.8)
+                    {
+                       if (this.isAChosenOne(i, j)) // noise(i, j) > 0.75)
+                        {
+                            continue;
+                        }
+                    }
+                    makeCircle(i*size-size/2, j*size-size/2, size); //clip with just the main canvas!  
+                    
+                    noFill();
+                    circle(i*size-size/2, j*size-size/2, size);   
                 }
             }
         }      
@@ -60,13 +108,15 @@ function Screens() {
         var r = size*0.5;
         push();
         noStroke();
+        strokeWeight(3);
+        stroke(255);
         translate(x, y);
         rotate(t*PI/2);
         translate(-x, -y);
         fill(50,50,200);
         circle(x,y,size);
         canvas.getContext("2d").clip();
-        fill(200,50,150)
+        fill(255,50,170)
         circle(x-r, y-r, size);
         circle(x+r, y-r, size);
         circle(x-r, y+r, size);
